9b4ce9dc34117940c6a58e12b0a69b384a108de3
Marcos Cervetto <cervettomarcos@gmail.com>
2023-12-22 09:17:34 -0300

    exercism leap

--- exercism/zig/leap/leap.zig
+++ exercism/zig/leap/leap.zig
@@ -1,12 +1,3 @@
 pub fn isLeapYear(year: u32) bool {
-    if ((year % 4) == 0) {
-        if (year % 400 == 0) {
-            return true;
-        }
-        if (year % 100 == 0) {
-            return false;
-        }
-        return true;
-    }
-    return false;
+    return ((year % 4) == 0 and (year % 100) != 0) or (year % 400 == 0);
 }
